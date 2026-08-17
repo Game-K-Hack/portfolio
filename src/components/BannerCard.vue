50307578181057ee0316887065bb157499f395f7
Game K <hack.h@laposte.net>
2026-08-17 04:40:41 +0200

    update: kanban

--- src/components/BannerCard.vue
+++ src/components/BannerCard.vue
@@ -10,7 +10,7 @@
         <div class="hidden md:flex w-[90%] justify-around">
             <template v-for="key in ['rank', 'room', 'level', 'badge']">
                 <div class="flex flex-col justify-center gap-2" v-if="CYBER[id][key] !== undefined">
-                    <p class="text-classic-p2 font-bold text-center">{{ CYBER[id][key].value }}</p>
+                    <p class="text-classic-p2 font-bold text-center">{{ CYBER[id][key] }}</p>
                     <p class="text-classic-m1 font-medium text-center">{{ $t(`cyber.${id}.${key}`) }}</p>
                 </div>
             </template>
@@ -19,7 +19,7 @@
             <template v-for="key in ['rank', 'room', 'level', 'badge']">
                 <template v-if="CYBER[id][key] !== undefined">
                     <p class="text-classic-m1 font-medium text-end col-1 content-center">{{ $t(`cyber.${id}.${key}`) }}</p>
-                    <p class="text-classic-p2 font-bold text-start col-2 pl-3">{{ CYBER[id][key].value }}</p>
+                    <p class="text-classic-p2 font-bold text-start col-2 pl-3">{{ CYBER[id][key] }}</p>
                 </template>
             </template>
         </div>
--- src/data/cyber.js
+++ src/data/cyber.js
@@ -1,15 +1,15 @@
 export const CYBER = {
     tryhackme: {
         url: "https://tryhackme.com/p/GameK", 
-        rank: { name: "Rang", value: "430885" }, 
-        room: { name: "Salle complété", value: "21" }, 
-        level: { name: "Niveau", value: "5" }, 
-        badge: { name: "Badges", value: "3" }, 
+        rank: "430885", 
+        room: "21", 
+        level: "5", 
+        badge: "3", 
     }, 
     rootme: {
         url: "https://www.root-me.org/Game-K", 
-        rank: { name: "Rang", value: "32816" }, 
-        room: { name: "Défis complété", value: "42" }, 
-        level: { name: "Score", value: "505" }, 
+        rank: "32816", 
+        room: "42", 
+        level: "505", 
     }
 }
